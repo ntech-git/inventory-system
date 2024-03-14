fad02d8b3946e19380d1e91590af52ba2bb7801d
Aqib Siddique <158513565+ntech-git@users.noreply.github.com>
2024-03-14 22:25:51 +0500

    handled signup and login

---
+++ Inventory-System/Inventory-System/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_System.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
